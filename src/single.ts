2a1d14966a007959d5cc00b0ab47b9eca64f6a4a
Christian Alexander <christian@linux.com>
2019-03-28 05:25:46 -0700

    feat: add single initializer wrapper

---
+++ src/single.ts
@@ -0,0 +1,15 @@
+import { Initializer, InitializerGetter } from "./registry";
+
+export function single<M, K extends keyof M>(
+  initializer: Initializer<M, K>
+): Initializer<M, K> {
+  let cachedValue: M[K];
+
+  return (get: InitializerGetter<M, keyof M>): M[K] => {
+    if (!cachedValue) {
+      cachedValue = initializer(get);
+    }
+
+    return cachedValue;
+  };
+}
