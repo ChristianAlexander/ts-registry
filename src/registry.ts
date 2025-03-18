44a84504ae0560dba1ac646ef4984e8de09e68ca
Steve Konves <stephen.konves@floqast.com>
2025-03-18 14:07:42 -0700

    docs: add documentation for the registry API

--- src/registry.ts
+++ src/registry.ts
@@ -35,7 +35,7 @@ export class Registry<M> implements IRegistry<M> {
     return instance;
   };
 
-  for = <K extends keyof M>(key: K) => {
+  for = <K extends keyof M>(key: K): ScopableInitializer<M, K> => {
     return {
       withScope: <TScope extends Scope>(
         scopeProvider: ScopeProvider<TScope>,
@@ -61,32 +61,40 @@ export class Registry<M> implements IRegistry<M> {
   private readonly instancesByScope = new WeakMap<object, Map<keyof M, any>>();
 }
 
-export type Scope = object | undefined
+export type ScopableInitializer<M, K extends keyof M> = {
+  /** Specifies an explicit scope for the service initializer. All calls to `get` will return an instance of the service based on this scope. */
+  withScope<TScope extends Scope>(
+    scopeProvider: ScopeProvider<TScope>,
+  ): {
+    /** Defines a service initializer with the explicitly specified scope. */
+    use: (i: ScopedInitializer<M, K, TScope>) => void;
+  };
+  /** Defines a service initializer. Implicitly uses a unique scope (a new instance of the service will be created for every resolution). */
+  use: (i: Initializer<M, K>) => void;
+};
+
+export type Scope = object | undefined;
 
 export interface IRegistry<M> {
+  /** Gets a service instance by key. The specific instance returned is controlled by the service's initilaizer scope. */
   get<K extends keyof M>(key: K): M[K];
-  for<K extends keyof M>(
-    key: K,
-  ): {
-    withScope<TScope extends Scope>(
-      scopeProvider: ScopeProvider<TScope>,
-    ): {
-      use: (i: ScopedInitializer<M, K, TScope>) => void;
-    };
-    use: (i: Initializer<M, K>) => void;
-  };
+  /** Defines the key to be used for a service. Chained methods will define the service scope and initializer. */
+  for<K extends keyof M>(key: K): ScopableInitializer<M, K>;
 }
 
 export type Initializer<M, K extends keyof M> = (
+  /** Gets a service instance by key. The specific instance returned is controlled by that service's initilaizer scope. */
   get: InitializerGetter<M>,
 ) => M[K];
 
 export type ScopedInitializer<M, K extends keyof M, TScope extends Scope> = (
+  /** Gets a service instance by key. The specific instance returned is controlled by that service's initilaizer scope. */
   get: InitializerGetter<M>,
+  /** The scope object (if any) associated with this instance of the service. */
   scope: TScope,
 ) => M[K];
 
-export type InitializerGetter<M> = <K extends keyof M>(key: K) => M[K]
+export type InitializerGetter<M> = <K extends keyof M>(key: K) => M[K];
 
 export interface ScopeProvider<TScope extends Scope> {
   /** Gets the scope where the instance will be stored after initialization */
@@ -96,11 +104,13 @@ export interface ScopeProvider<TScope extends Scope> {
 }
 
 const singletonScope = Object.freeze({});
+/** Ensures that a single service is instantiated once and then shared across all resolutions. */
 export const singleton: ScopeProvider<typeof singletonScope> = {
   getTargetScope: () => singletonScope,
   sourceScopeGetters: [() => singletonScope],
 };
 
+/** Ensures that a new instance of a service is created for every resolution. */
 export const unique: ScopeProvider<undefined> = {
   getTargetScope: () => undefined,
   sourceScopeGetters: [],
